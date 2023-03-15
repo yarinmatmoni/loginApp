7f56681d03580e88f244fd9cf2d170330c776e70
yarinmatmoni <yarinmatmoni@gmail.com>
2023-03-15 15:34:11 +0200

    clean project + add routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './styles/globalStyle.scss';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { routes } from './utils/browserRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(routes);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router}>
+			<App />
+		</RouterProvider>
+	</React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ frontend/src/utils/browserRoutes.js
@@ -0,0 +1,5 @@
+export const routes = [
+	{ path: '/', element: <div>Root</div> },
+	{ path: '/register', element: <div>register</div> },
+	{ path: '*', element: <div>page not found</div> },
+];
