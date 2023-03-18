ac51c896b7b31478084ec80a6c40aa86144a367a
yarinmatmoni <yarinmatmoni@gmail.com>
2023-03-18 14:55:13 +0200

    refactor recover component

--- frontend/src/components/recover/Recover.js
+++ frontend/src/components/recover/Recover.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './Recover.module.scss';
+import { Toaster } from 'react-hot-toast';
+import { useFormikConfig } from '../../hooks/useFormik';
+import { OTPValidation } from '../../utils/validate';
 
 function Recover() {
+	const formik = useFormikConfig(['OTP'], OTPValidation);
+
 	return (
 		<div className={style.recoverContainer}>
+			<Toaster
+				position='top-center'
+				reverseOrder={false}
+			></Toaster>
 			<div className={style.titles}>
 				<h1>Recovery</h1>
-				<p>Enter OTP to recover your password</p>
+				<p>Enter OTP digits who sent to your email to recover your password</p>
 			</div>
-			<form>
+			<form onSubmit={formik.handleSubmit}>
 				<div className={style.formInputs}>
 					<div className={style.inputContainer}>
 						<input
 							type='text'
 							placeholder='OTP'
+							{...formik.getFieldProps('OTP')}
 						></input>
 					</div>
 					<button type='submit'>RECOVER</button>
@@ -22,7 +32,7 @@ function Recover() {
 				</div>
 				<div className={style.bottom}>
 					<p>
-						Can't fet OTP? <Link to={'/'}>Resend Now</Link>
+						Can't get OTP? <Link to={'/'}>Resend Now</Link>
 					</p>
 				</div>
 			</form>
--- frontend/src/utils/validate.js
+++ frontend/src/utils/validate.js
@@ -5,8 +5,20 @@ export const loginValidation = async (values) => {
 	return errors;
 };
 
+export const OTPValidation = async (values) => {
+	const errors = OTPVerify({}, values);
+	return errors;
+};
+
 const loginVerify = (error = {}, values) => {
 	if (!values.email) error.email = toast.error('Email Required');
 	else if (!values.password) error.password = toast.error('Password Required');
+	return error;
+};
+
+//TODO: add check the value is only digits
+const OTPVerify = (error = {}, values) => {
+	if (!values.OTP) error.email = toast.error('OTP Digits Required');
+	else if (values.OTP.length !== 6) error.OTP = toast.error('OTP Include 6 Digits Only');
 	return error;
 };
