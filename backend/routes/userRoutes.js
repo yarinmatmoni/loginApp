b5b7db715af0a1bba0d388a825d2077960f27b43
yarinmatmoni <yarinmatmoni@gmail.com>
2023-03-26 11:23:34 +0300

    reset password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const User = require('../controllers/user');
 const Auth = require('../common/authMiddleware');
+const VerifyUser = require('../common/verifyUserMiddleWare');
 
 router.get('/:userName', User.getUser);
 
 router.put('/updateUser/', Auth, User.updateUser);
+
+router.put('/resetPassword', VerifyUser, User.resetPassword);
 
 module.exports = router;
